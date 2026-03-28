71e3cfb01e2935c3e0a6ff54c9d033db44cef5c9
Deepak656 <deepak01962@gmail.com>
2026-03-28 10:37:50 +0530

    jpa hibernate auto-ddl create

--- sql_when_no_flyway/V1_change_in_email_purpose_enum.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE email_otps
-DROP CONSTRAINT email_otps_purpose_check;
-
-ALTER TABLE email_otps
-ADD CONSTRAINT email_otps_purpose_check
-CHECK (purpose IN ('SIGNUP', 'RESET_PASSWORD', 'CHANGE_EMAIL', 'VERIFY_CURRENT_EMAIL'));
