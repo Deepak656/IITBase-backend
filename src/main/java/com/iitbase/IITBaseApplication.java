959577c562a75870920a3a63b6d8dfa84458796c
Deepak656 <deepak01962@gmail.com>
2026-03-30 02:37:06 +0530

    redis debug

--- src/main/java/com/iitbase/IITBaseApplication.java
+++ src/main/java/com/iitbase/IITBaseApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-@SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class
-        }
-)
 public class IITBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(IITBaseApplication.class, args);
