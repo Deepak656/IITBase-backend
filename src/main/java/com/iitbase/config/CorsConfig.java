abbd9bd438e4dafa07107185e195be0924047c78
Deepak656 <deepak01962@gmail.com>
2026-04-16 00:59:48 +0530

    fix: add https//www.iitbase.com in cors config

--- src/main/java/com/iitbase/config/CorsConfig.java
+++ src/main/java/com/iitbase/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://iitbase.com", "http://172.20.10.2:3000", "https://staging.iitbase.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://iitbase.com", "https://iitbase.com", "http://172.20.10.2:3000", "https://staging.iitbase.com"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
